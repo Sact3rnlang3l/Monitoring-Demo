ea3884885976215dc3673ee82c06d726cec2131f
Aiden gillespie <aiden@cold.org>
2021-08-31 11:27:10 -0600

    reinstalled npm dependencies

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express")
-// const { ConsoleWriter } = require("istanbul-lib-report")
 const path = require("path")
+
 const app = express()
 
 app.get("/", (req, res) => {
