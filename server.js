0697c803d10e449e5984ffd70768f9563b7eb480
Aiden gillespie <aiden@cold.org>
2021-08-31 12:35:24 -0600

    Added further functionality

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ let rollbar = new Rollbar({
 
 const students = []
 const app = express()
-
-
+app.use('/style', express.static('/public/styles.css'))
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/server/index.html"))
@@ -21,13 +20,21 @@ app.get("/", (req, res) => {
 })
 app.post("/api/student", (req, res) => {
   let { name } = req.body
-  name = name.trim
+  name = name.trim()
 
-  rollbar.log("Meme Lord Added to Hall of Fame", {
-    author: "Aiden",
-    type: "manual entry",
-  })
-  res.sendStatus(200).send(students)
+  const index = students.findIndex((studentName) => studentName === name)
+
+  if (index === -1 && name !== "") {
+    students.push(name)
+    rollbar.log("Student added")
+    res.status(200).send(students)
+  } else if (name === "") {
+      rollbar.error('no name given')
+    res.status(400).send("must provide a name.")
+  } else {
+      rollbar.error('Student already exists')
+    res.status(400).send("that student already exists")
+  }
 })
 
 app.use(rollbar.errorHandler())
