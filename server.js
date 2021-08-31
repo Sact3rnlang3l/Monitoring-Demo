1f6f90865f427505cb4ca34a95e9ab94369e523d
Aiden gillespie <aiden@cold.org>
2021-08-31 11:18:40 -0600

    Added proc and more

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { ConsoleWriter } = require('istanbul-lib-report')
 const path = require('path')
 const app = express()
 
@@ -6,3 +7,5 @@ app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname,'/server/index.html'))
 })
 app.listen(4040, () => console.log('Loud and Clear on 4040'))
+const port = process.env.PORT || 4040
+app.listen(port, ()=> console.log(`Loud and clear on ${port}`))
